df59c6e2622bc3f85ac70a45a804c2f5eab7d8d4
XEZORB <149890797+XEZORB@users.noreply.github.com>
2025-08-10 00:07:59 -0400

    Create MainActivity.java

---
+++ app/src/main/java/com/example/cashdroid/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cashdroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button sendMoneyButton = findViewById(R.id.sendMoneyButton);
+        sendMoneyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SendMoneyActivity.class));
+            }
+        });
+    }
+}
